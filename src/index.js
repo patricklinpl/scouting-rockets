967ba670d57625f353519b2f3f4f959bc319f66c
Patrick Lin <patrick@patricklin.ca>
2018-11-05 01:50:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 
 /** Entry point of React Application - Renders App.js */
 ReactDOM.render(<App />, document.getElementById('root'))
-// registerServiceWorker()
+registerServiceWorker()
